88c0d31439d460b96a12ecb8d87673cd140f6044
Noé Gnanih <noegnanih@gmail.com>
2024-08-05 09:10:54 +0000

    fix: add style import in playground

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import Vue3MapChart from 'vue3-map-chart'
-import 'vue3-map-chart/dist/style.css'
 
 import App from './App.vue'
+
+import(import.meta.env.DEV ? './style.css' : 'vue3-map-chart/dist/style.css')
 
 createApp(App).use(Vue3MapChart).mount('#app')
